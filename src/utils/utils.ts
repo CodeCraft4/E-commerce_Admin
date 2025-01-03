23b830d2b14787429cbf638a306274f5f97f060a
Imad Shah <shahimad499@gmail.com>
2025-01-03 18:19:53 +0500

    feat: add styles and theme modifications

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,1 +1,31 @@
-export {}
+import { COLORS } from "@muc/constants";
+
+export const dragAndDrop= {
+    width: "441px",
+    height: "350px",
+    bgcolor: COLORS.gray.lightGray,
+    borderRadius: "10px",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    cursor: "pointer",
+    textAlign: "center",
+    position: "relative",
+}
+
+export const dragAndDropGallary={
+    border: `2px dashed ${COLORS.gray.darkGray}`,
+    borderRadius: "10px",
+    width: "100%",
+    maxWidth: "401px",
+    p: 3,
+    my: 2,
+    height: 150,
+    cursor: "pointer",
+    textAlign: "center",
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center",
+    position: "relative",
+}
