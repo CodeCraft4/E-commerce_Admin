768087a4bd0e1895354bc8488d401e19db99d4f6
Imad Shah <shahimad499@gmail.com>
2025-01-07 15:57:29 +0500

    fix: navigate

--- src/modules/ManageAuth/components/SignInForm/SignInForm.tsx
+++ src/modules/ManageAuth/components/SignInForm/SignInForm.tsx
@@ -9,11 +9,8 @@ import {
 import { CustomButton, CustomTextField } from "@muc/components";
 import { COLORS, ROUTES } from "@muc/constants";
 import { ArrowForwardIosOutlined } from "@mui/icons-material";
-<<<<<<< HEAD
-=======
 import { loginSchema } from "@muc/validations";
 import { yupResolver } from "@hookform/resolvers/yup";
->>>>>>> 1d107c53bb47c2490e73813cacce708fd9fd2123
 import { useNavigate } from "react-router-dom";
 
 interface SignUpFormFields {
@@ -22,13 +19,6 @@ interface SignUpFormFields {
 }
 
 const SignInForm = () => {
-<<<<<<< HEAD
-  const methods = useForm<SignUpFormFields>();
-  const navigate = useNavigate();
-
-  const onSubmit = (e: SignUpFormFields) => {
-    console.log(e, "Log in");
-=======
   const methods = useForm<SignUpFormFields>({
     resolver: yupResolver(loginSchema),
   });
@@ -37,7 +27,6 @@ const SignInForm = () => {
 
   const onSubmit = (data: SignUpFormFields) => {
     console.log(data.email.split("@")[0], "Welcome to DASHBOARD");
->>>>>>> 1d107c53bb47c2490e73813cacce708fd9fd2123
     navigate(ROUTES.ADMIN.DASHBOARD);
   };
 
